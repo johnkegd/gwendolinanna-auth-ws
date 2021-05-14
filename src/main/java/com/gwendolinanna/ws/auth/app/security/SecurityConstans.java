6b8143ad03c203d336ca3402860659440ea7dfc8
John Garcia <john.garcia@ajila.com>
2021-05-14 21:59:50 +0200

    added  filter to security sign-in test

--- src/main/java/com/gwendolinanna/ws/auth/app/security/SecurityConstans.java
+++ src/main/java/com/gwendolinanna/ws/auth/app/security/SecurityConstans.java
@@ -6,7 +6,7 @@ package com.gwendolinanna.ws.auth.app.security;
 public class SecurityConstans {
     public static final long EXPIRATION_TIME = 864000000; // 10 days
     public static final String TOKEN_PREFIX = "";
-    public static final String HEADER_STRING ="";
+    public static final String HEADER_STRING ="Bearer";
     public static final String SING_UP_URL = "/users";
     public static final String TOKEN_SECRET = "jjasdoi123jasld";
 }
--- src/main/java/com/gwendolinanna/ws/auth/app/security/WebSecurity.java
+++ src/main/java/com/gwendolinanna/ws/auth/app/security/WebSecurity.java
@@ -24,7 +24,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests().
         antMatchers(HttpMethod.POST, SecurityConstans.SING_UP_URL).
-        permitAll().anyRequest().authenticated();
+        permitAll().anyRequest().authenticated().and().addFilter(new AuthenticationFilter(authenticationManager()));
     }
 
     @Override
