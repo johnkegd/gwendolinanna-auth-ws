a1bc0fa8e758b563ed75092f74eb731f3f51b290
John Garcia <john.garcia@ajila.com>
2021-05-16 18:32:47 +0200

    triying deploy in AWS instance

--- src/main/java/com/gwendolinanna/ws/auth/app/GwendolinannaWsAuthApplication.java
+++ src/main/java/com/gwendolinanna/ws/auth/app/GwendolinannaWsAuthApplication.java
@@ -2,11 +2,18 @@ package com.gwendolinanna.ws.auth.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class GwendolinannaWsAuthApplication {
+public class GwendolinannaWsAuthApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GwendolinannaWsAuthApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GwendolinannaWsAuthApplication.class, args);
