8e84c54d3b339b683ca3a549b8119698691f929c
John Garcia <johnkegd@gmail.com>
2021-08-08 22:14:15 +0200

    roles: added authority repository and role repository

---
+++ src/main/java/com/gwendolinanna/ws/auth/app/io/repositories/AuthorityRepository.java
@@ -0,0 +1,16 @@
+package com.gwendolinanna.ws.auth.app.io.repositories;
+
+import com.gwendolinanna.ws.auth.app.io.entity.AuthorityEntity;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author Johnkegd
+ */
+@Repository
+public interface AuthorityRepository extends CrudRepository<AuthorityEntity, Long> {
+
+    AuthorityEntity findByName(String name);
+
+}
